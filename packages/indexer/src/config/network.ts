c9b062fa98a64c30211625bcf19a5f76075aa167
Nikunj goyal <40403668+Nike682631@users.noreply.github.com>
2024-04-05 19:16:31 +0530

    Updating decimals for METH

--- packages/indexer/src/config/network.ts
+++ packages/indexer/src/config/network.ts
@@ -496,7 +496,7 @@ export const getNetworkSettings = (): NetworkSettings => {
               contract: "0xed5464bd5c477b7f71739ce1d741b43e932b97b0",
               name: "BAP Methane",
               symbol: "METH",
-              decimals: 0,
+              decimals: 18,
               metadata: {
                 image: "https://i.ibb.co/Mc5Pmjn/baptoken.png",
               },
